3a6cddc0306032d185e6f999ef26fb9c9169ab62
edison166 <edison.zhao@bomoda.com>
2017-10-18 15:56:45 -0400

    runnable

--- train.py
+++ train.py
@@ -24,6 +24,8 @@ if not MODEL_OUTPUT_FILE:
 # Load data
 x_train, y_train, word_to_index, index_to_word = load_data(INPUT_DATA_FILE, VOCABULARY_SIZE)
 
+
+
 # Build model
 model = GRUTheano(VOCABULARY_SIZE, hidden_dim=HIDDEN_DIM, bptt_truncate=-1)
 
@@ -33,6 +35,7 @@ model.sgd_step(x_train[10], y_train[10], LEARNING_RATE)
 t2 = time.time()
 print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
 sys.stdout.flush()
+
 
 # We do this every few examples to understand what's going on
 def sgd_callback(model, num_examples_seen):
@@ -46,7 +49,41 @@ def sgd_callback(model, num_examples_seen):
   print("\n")
   sys.stdout.flush()
 
+def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5, decay=0.9, 
+    callback_every=PRINT_EVERY, callback=sgd_callback):
+    # We keep track of the losses so we can plot them later
+    losses = []
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # Optionally evaluate the loss
+        if (epoch % evaluate_loss_after == 0):
+            loss = model.calculate_loss(X_train, y_train)
+            losses.append((num_examples_seen, loss))
+            time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
+            # Adjust the learning rate if loss increases
+            if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
+                learning_rate = learning_rate * 0.5  
+                print "Setting learning rate to %f" % learning_rate
+            sys.stdout.flush()
+            # ADDED! Saving model oarameters
+            #save_model_parameters_theano("./data/rnn-theano-%d-%d-%s.npz" % (model.hidden_dim, model.word_dim, time), model)
+        # For each training example...
+        for i in range(len(y_train)):
+            # One SGD step
+            model.sgd_step(X_train[i], y_train[i], learning_rate)
+            num_examples_seen += 1
+            
+            loss = model.calculate_loss(X_train, y_train)
+            print loss
+
+
+
 for epoch in range(NEPOCH):
-  train_with_sgd(model, x_train, y_train, learning_rate=LEARNING_RATE, nepoch=1, decay=0.9, 
+  train_with_sgd(model, x_train, y_train, learning_rate=LEARNING_RATE, nepoch=1, evaluate_loss_after=5, decay=0.9, 
     callback_every=PRINT_EVERY, callback=sgd_callback)
+
+
+
+
 
